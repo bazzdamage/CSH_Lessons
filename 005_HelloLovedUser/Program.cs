0859795988d6e5da5bf6e07d9237d78a93b799f6
bazzdamage <bazzdamage@outlook.com>
2022-02-23 03:34:37 +0700

    Add 005_HelloLovedUser

---
+++ 005_HelloLovedUser/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Input your username");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "maria")
+{
+    Console.WriteLine("Hooray, its a Maria");
+}
+else
+{
+    Console.Write("Hello, ");
+    Console.WriteLine(username);
+}
