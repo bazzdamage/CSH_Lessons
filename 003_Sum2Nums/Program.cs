f11b349d2e7da1af3e77f3c213aa3e8e7026564a
bazzdamage <bazzdamage@outlook.com>
2022-02-23 03:26:52 +0700

    003_Sum2Nums - add random nums

--- 003_Sum2Nums/Program.cs
+++ 003_Sum2Nums/Program.cs
@@ -1,3 +1,8 @@
-﻿int numA = 3;
-int numB = 5; 
-Console.WriteLine("Sum of 2 nums = " + numA + numB);
+﻿int numA = new Random().Next(1,10); // 1 2 3 4 .... 9
+Console.Write("Random Num A = ");
+Console.WriteLine(numA);
+int numB = new Random().Next(1,10);
+Console.Write("Random Num B = ");
+Console.WriteLine(numB);
+Console.Write("Sum of Nums A & B = ");
+Console.WriteLine(numA + numB);
